5d13b161d4d04f048fe6853c6626e0d78336151a
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-08-11 14:15:52 +1000

    Perform an HTTP check with erlang httpc

--- api/lib/api/check_handlers/api/check_handlers/endpoint_checker.ex
+++ api/lib/api/check_handlers/api/check_handlers/endpoint_checker.ex
@@ -4,6 +4,17 @@ defmodule Api.CheckHandlers.EndpointChecker do
   require Logger
 
   def check({name, id, url}) do
-    Logger.debug("Pinging #{name} (#{id}) on #{url}")
+    Logger.debug("Pinging #{name} (id=#{id}) on #{url}")
+    check_result = :httpc.request(to_charlist url)
+
+    handle_result(check_result, name, id)
+  end
+
+  defp handle_result({:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}}, name, _id) do
+    Logger.debug("OK #{name}")
+  end
+
+  defp handle_result({:error, {:failed_connect, _params}}, name, _id) do
+    Logger.debug("FAIL #{name}")
   end
 end
