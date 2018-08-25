66d7993d729ea77d0e88121342e9d588accbb590
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-08-25 15:18:56 +1000

    Refactor Resources.Manifest with more pattern matching

--- api/lib/api/resources/manifest.ex
+++ api/lib/api/resources/manifest.ex
@@ -16,54 +16,34 @@ defmodule Api.Resources.Manifest do
     end
   end
 
-  defp ensure_api_version({_, params}) do
-    cond do
-      !is_map(params) ->
-        {:bad_request, %{message: "params is not a map"}}
-      !Map.has_key?(params, "apiVersion") ->
-        {:bad_request, %{message: "params is missing apiVersion"}}
-      Map.get(params, "apiVersion") == 1 ->
-        {:ok, params}
-      true ->
-        {:bad_request, %{message: "only 'apiVersion: 1' is supported for now"}}
-    end
-  end
+  defp ensure_api_version({:ok, %{"apiVersion" => nil} = _}), do: {:bad_request, %{message: "Missing apiVersion"}}
+  defp ensure_api_version({:ok, %{"apiVersion" => 1} = params}), do: {:ok, params}
+  defp ensure_api_version({_, _}), do: {:bad_request, %{message: "only 'apiVersion: 1' is supported for now"}}
 
-  defp ensure_kind_present({:ok, %{"kind" => _} = params}) do
-    {:ok, params}
-  end
+  defp ensure_kind_present({:ok, %{"kind" => _} = params}), do: {:ok, params}
   defp ensure_kind_present({:bad_request, params}), do: {:bad_request, params}
-  defp ensure_kind_present({_, _}) do
-    {:bad_request, %{message: "Missing kind"}}
-  end
+  defp ensure_kind_present({_, _}), do: {:bad_request, %{message: "Missing kind"}}
 
-  defp ensure_kind_valid({status, params}) do
-    if status == :bad_request do
-      {status, params}
+  defp ensure_kind_valid({:bad_request, params}), do: {:bad_request, params}
+  defp ensure_kind_valid({status, %{"kind" => kind} = params}) do
+    if Enum.member?(@acceptable_kinds, kind) do
+      {status, kind, params}
     else
-      %{"kind" => kind} = params
-
-      if Enum.member?(@acceptable_kinds, kind) do
-        {status, kind, params}
-      else
-        {:bad_request, %{message: "Invalid kind '#{kind}'. Acceptable values are #{Enum.join(@acceptable_kinds, ", ")}"}}
-      end
+      {
+        :bad_request,
+        %{message: "Invalid kind '#{kind}'. Acceptable values are #{Enum.join(@acceptable_kinds, ", ")}"}
+      }
     end
   end
 
-  defp ensure_spec_present({status, kind, params}) do
-    cond do
-      status == :bad_request ->
-        {status, kind, params}
-      Map.has_key?(params, "spec") ->
-        {:ok, kind, params}
-      true ->
-        {:bad_request, %{message: "Missing spec"}}
+  defp ensure_spec_present({:bad_request, kind, params}), do: {:bad_request, kind, params}
+  defp ensure_spec_present({:bad_request, message}), do: {:bad_request, message}
+  defp ensure_spec_present({_, kind, params}) do
+    if Map.has_key?(params, "spec") && is_map(params["spec"]) do
+      {:ok, kind, params}
+    else
+      {:bad_request, %{message: "Missing spec"}}
     end
-  end
-
-  defp ensure_spec_present({:bad_request, message}) do
-    {:bad_request, message}
   end
 
   defp validate(params) do
