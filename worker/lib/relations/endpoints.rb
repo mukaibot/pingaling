968edf6ec2b8c5e32b0cddf444efd625e3babc68
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-08-25 15:06:26 +1000

    Remove the unused Ruby worker dir

--- worker/lib/relations/endpoints.rb
+++
@@ -1,7 +0,0 @@
-module Relations
-  class Endpoints < ROM::Relation[:sql]
-    schema(:endpoints, infer: true)
-    struct_namespace '::Entities'
-    auto_struct true
-  end
-end
--- worker/lib/result_updater.rb
+++
@@ -1,9 +0,0 @@
-class ResultUpdater
-  def call(id:, type:, check_result:)
-    @id = id
-    @type = type
-    @check_result = check_result
-
-    true
-  end
-end
