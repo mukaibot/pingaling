980c0d7e3a8ea7744c243dc3bfaf991af57c8848
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-09-07 09:08:29 +1000

    Log the database URL

--- api/lib/api/repo.ex
+++ api/lib/api/repo.ex
@@ -1,5 +1,6 @@
 defmodule Api.Repo do
   use Ecto.Repo, otp_app: :api
+  require Logger
 
   @doc """
   Dynamically loads the repository url from the
@@ -11,6 +12,8 @@ defmodule Api.Repo do
     database = System.get_env("POSTGRES_DB") || "pingaling"
     hostname = System.get_env("POSTGRES_HOST") || "localhost"
     url      = System.get_env("DATABASE_URL") || "postgresql://#{username}@#{hostname}/#{database}"
+
+    Logger.debug("Connecting to database #{url}")
 
     {
       :ok,
