420e8f860d75f37136d7f4368aae82ab4a95759b
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-09-13 06:06:46 +1000

    Improve formatting of Slack messages

--- api/lib/api/notification_channels/slack.ex
+++ api/lib/api/notification_channels/slack.ex
@@ -3,14 +3,16 @@ defmodule Api.NotificationChannels.Slack do
   require Poison
   require HTTPoison
 
+  @slack_name "Pingaling Monitoring"
+
   def send({:open, :endpoint, %{incident: incident, endpoint: endpoint, url: webhook_url}}) do
-    message = Poison.encode!(%{text: "New alert for #{endpoint.name} (id=#{incident.id})"})
+    message = Poison.encode!(%{username: @slack_name, text: ":fire: New alert for #{endpoint.name} (id=#{incident.id}). #{endpoint.name} is UNHEALTHY"})
     Logger.info(message)
     HTTPoison.post(webhook_url, message)
   end
 
   def send({:auto_resolved, :endpoint, %{incident: incident, endpoint: endpoint, url: webhook_url}}) do
-    message = Poison.encode!(%{text: "Incident for #{endpoint.name} (id=#{incident.id}) resolved by system"})
+    message = Poison.encode!(%{username: @slack_name, text: ":green: Incident for #{endpoint.name} (id=#{incident.id}) resolved by system"})
     Logger.info(message)
     HTTPoison.post(webhook_url, message)
   end
