fc880b8f07008c3b48eccabcf5d023d0bec87d02
Timothy Mukaibo <timothy.mukaibo@rea-group.com>
2018-09-24 07:47:55 +1000

    API: fix tests running outside of Docker

--- api/config/test.exs
+++ api/config/test.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
+System.put_env("POSTGRES_DB", "pingaling_test")
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :api, ApiWeb.Endpoint,
